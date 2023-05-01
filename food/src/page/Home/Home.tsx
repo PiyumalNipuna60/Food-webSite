5774485c6e46a03920556d4b00efa4fb467d5151
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-02 00:49:44 +0530

    Add Header component

--- food/src/page/Home/Home.tsx
+++ food/src/page/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from '../../components/Header/Header'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="">
         <h1 className="bg-black text-red-800 border-b-4 cursor-pointer border-accent-main text-accent-main">Home</h1>
+        <Header />
       </div>
     )
   }
