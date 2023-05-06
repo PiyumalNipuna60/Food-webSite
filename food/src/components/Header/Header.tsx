9931d84a439eefa9a7dd5117a0a99f7f546fda16
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-05-06 20:21:46 +0530

    Add Content

--- food/src/components/Header/Header.tsx
+++ food/src/components/Header/Header.tsx
@@ -6,16 +6,16 @@ import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
-    const activeLink="border-b-4 cursor-pointer hover:border-accent-main text-accent-main";
-     const activeLink2="border-2 cursor-pointer hover:border-accent-main rounded-full";
-    const normalLink="cursor-pointer hover:bg-red-100 hover:text-accent-main";
+    const activeLink="border-b-4 cursor-pointer hover:border-accent-main text-accent-set-200";
+     const activeLink2="border-2 cursor-pointer hover:border-accent-set-200 rounded-full";
+    const normalLink="cursor-pointer hover:bg-red-100 hover:text-accent-set-200";
 
     return (
         <div className=" w-11/12 h-13 text-black fixed p-4 z-10 m-5 rounded-full snap-center ml-16 shadow-xl border-2">
       <div className="w-full h-full">
           <div className="flex w-full h-full ">
               <div className="w-1/3  items-center ">
-                  <h1 className="font-babylonica text-2xl md:font-bold  text-accent-main">Food.com</h1>
+                  <h1 className="font-babylonica text-2xl md:font-bold  text-accent-set-200">Food.com</h1>
               </div>
               <div className="w-2/3">
                   <nav className="flex w-full space-x-20 md:font-bold text-xl  font-heading pl-10">
